555ebdedbb74e7d47ddd9fd8d36bc7a814d72b12
emmanishikawa <emmanishikawa@gmail.com>
2026-04-25 16:51:30 -0700

    add bg color and disable scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-center bg-gray-100">
-      <div className="w-full max-w-sm bg-white min-h-screen">
-        <h1 className="text-center mt-10">marumemo</h1>
+    <main className="min-h-screen flex justify-center">
+      <div className="w-full max-w-sm min-h-screen">
+        <h1 className="text-center mt-10 text-black">marumemo</h1>
       </div>
     </main>
   );
