617f1e7f48e81f36e6dba8ba3b9047a67d21a697
emmanishikawa <emmanishikawa@gmail.com>
2026-04-25 18:17:11 -0700

    landing page prototype todo: fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
+//TODO FIX LAYOUT
+
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-start h-full">
-      <h1 className="text-center my-40 text-[40px] tracking-[30%]">marumemo</h1>
-      <button className="w-46.25 h-20 justify-center
+    <main className="flex flex-col items-center justify-start h-full">
+      {/* title group */}
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-center text-[40px] tracking-[30%]">marumemo</h1>
+        <h2　className="text-center text-[20px] tracking-[250%]">マルメモ</h2>
+      </div>
+
+      {/* create button */}
+      <button className="w-46.25 h-20 gjustify-center
         bg-(--secondary) active:bg-(--primary) 
       text-white text-[25px] 
         border-2 border-solid border-(--primary) rounded-[10px] ">
           create
       </button>
-    </div>
+    </main>
       
     </>
   );
