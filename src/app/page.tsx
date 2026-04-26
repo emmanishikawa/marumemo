4832ef105669d6a37f1d3530ad2e320c6ad6b37b
emmanishikawa <emmanishikawa@gmail.com>
2026-04-25 18:35:23 -0700

    create working next page button to edit page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 //TODO FIX LAYOUT
+"use client";
+
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <>
     <main className="flex flex-col items-center justify-start h-full">
@@ -14,7 +18,8 @@ export default function Home() {
       <button className="w-46.25 h-20 gjustify-center
         bg-(--secondary) active:bg-(--primary) 
       text-white text-[25px] 
-        border-2 border-solid border-(--primary) rounded-[10px] ">
+        border-2 border-solid border-(--primary) rounded-[10px]"
+        onClick={() => router.push("/edit")}>
           create
       </button>
     </main>
