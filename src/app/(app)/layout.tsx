c1aff7955068d28fb72a8389a638756236bb96cb
emmanishikawa <emmanishikawa@gmail.com>
2026-04-25 18:35:57 -0700

    move files to separate layout dimension and image issues

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,16 @@
+export default function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      className="min-h-screen flex justify-center bg-center bg-contain bg-no-repeat"
+      style={{ backgroundImage: "url('/background/machine_bg.png')" }}
+    >
+      <div className="w-full max-w-sm min-h-screen">
+        {children}
+      </div>
+    </div>
+  );
+}
