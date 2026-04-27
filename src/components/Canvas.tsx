cf46dde4780895122ee22dd6a2ab6cf4d074d6f4
emmanishikawa <emmanishikawa@gmail.com>
2026-04-26 23:25:04 -0700

    capsule modal drawing and image add

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import { useRef, useEffect, useState } from "react";
+
+type Props = {
+  width?: number;
+  height?: number;
+  initialData?: string;
+  onSave?: (dataUrl: string) => void;
+};
+
+export default function Canvas({
+  width = 300,
+  height = 250,
+  initialData,
+  onSave,
+}: Props) {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const isDrawing = useRef(false);
+
+  const [hue, setHue] = useState(0);
+  const color = `hsl(${hue}, 100%, 40%)`;
+
+  const history = useRef<ImageData[]>([]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    ctx.lineWidth = 3;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = color;
+  }, []);
+  
+
+  function getPos(e: any) {
+    const canvas = canvasRef.current!;
+    const rect = canvas.getBoundingClientRect();
+
+    const x = (e.touches ? e.touches[0].clientX : e.clientX) - rect.left;
+    const y = (e.touches ? e.touches[0].clientY : e.clientY) - rect.top;
+
+    return { x, y };
+  }
+
+  function start(e: any) {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+
+    const { x, y } = getPos(e);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+    isDrawing.current = true;
+  }
+
+  function draw(e: any) {
+    if (!isDrawing.current) return;
+
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+
+    ctx.strokeStyle = color;
+    const { x, y } = getPos(e);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+
+    function end() {
+        isDrawing.current = false;
+
+        const canvas = canvasRef.current;
+        const ctx = canvas?.getContext("2d");
+        if (!canvas || !ctx) return;
+
+        history.current.push(
+            ctx.getImageData(0, 0, canvas.width, canvas.height)
+        );
+    }
+
+  function clear() {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (!ctx || !canvas) return;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  useEffect(() => {
+  if (!initialData) return;
+
+  const canvas = canvasRef.current;
+  if (!canvas) return;
+
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+
+  const img = new Image();
+  img.src = initialData;
+
+  img.onload = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0);
+  };
+}, [initialData]);
+
+function undo() {
+  const canvas = canvasRef.current;
+  const ctx = canvas?.getContext("2d");
+  if (!canvas || !ctx) return;
+
+  if (history.current.length === 0) return;
+
+  history.current.pop();
+
+  const last = history.current[history.current.length - 1];
+
+  if (!last) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    return;
+  }
+
+  ctx.putImageData(last, 0, 0);
+}
+
+  function save() {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const dataUrl = canvas.toDataURL("image/png");
+    onSave?.(dataUrl);
+  }
+
+  return (
+    <div>
+        <canvas
+            ref={canvasRef}
+            width={width}
+            height={height}
+            className="border rounded touch-none"
+            onMouseDown={start}
+            onMouseMove={draw}
+            onMouseUp={end}
+            onMouseLeave={end}
+            onTouchStart={start}
+            onTouchMove={draw}
+            onTouchEnd={end}
+        />
+
+        <input
+            type="range"
+            min="0"
+            max="360"
+            value={hue}
+            onChange={(e) => setHue(Number(e.target.value))}
+            className="w-full mt-2 hue-slider"
+        />
+
+        <div className="flex gap-2 mt-2">
+            <button onClick={clear}>Clear</button>
+            <button onClick={undo}>Undo</button>
+            <button onClick={save}>Save</button>
+        </div>
+
+        
+    </div>
+  );
+}
--- src/components/CapsuleModal.tsx
+++ src/components/CapsuleModal.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import type { Capsule } from "@/src/types/capsule";
+import Canvas from "./Canvas";
 
 type Props = {
     capsule: Capsule;
@@ -15,6 +16,19 @@ export default function CapsuleModal({ capsule, onClose, onSave }: Props) {
     const [trinket, setTrinket] = useState(capsule.trinket);
     const [images, setImages] = useState(capsule.images);
 
+    const canvasRef = useRef<any>(null);
+    
+    function save() {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const dataUrl = canvas.toDataURL("image/png");
+
+        console.log("saved:", dataUrl);
+
+        setTrinket(dataUrl);
+    }
+
     function handleSave() {
         onSave({
             ...capsule,
@@ -25,44 +39,112 @@ export default function CapsuleModal({ capsule, onClose, onSave }: Props) {
         onClose();
     }
 
+    function handleUpload(e: React.ChangeEvent<HTMLInputElement>) {
+        const file = e.target.files?.[0];
+        if (!file) return;
+
+        if (images.length >= 10) return;
+
+        const url = URL.createObjectURL(file);
+
+        setImages(prev => [
+            ...prev,
+            {
+            id: crypto.randomUUID(),
+            url,
+            caption: "",
+            },
+        ]);
+    }
+
     return (
-        <div className="fixed inset-0 bg-(--primary)/50 flex items-center justify-center">
-        <div className="bg-white p-6 w-100 rounded-xl">
+        <div className="fixed inset-0 flex items-center justify-center
+            bg-(--primary)/50 p-4">
+            <div className="bg-white p-6 w-100 rounded-xl">
 
-            <h2 className="text-xl mb-4">Edit Capsule</h2>
+                <h2 className="text-xl mb-4">Edit Capsule</h2>
 
-            <p>Capsule ID: {capsule.id}</p>
+                <p>Capsule ID: {capsule.id}</p>
 
-            {step === 1 && (
-            <div>
-                <p>Step 1: Draw Trinket</p>
-
-                <button onClick={() => setStep(2)}>
-                Next
-                </button>
-            </div>
-            )}
-
-            {step === 2 && (
+                {step === 1 && (
                 <div>
-                    <p>Step 2: Add Images</p>
+                    <p>Step 1: Draw Trinket</p>
 
-                    <button onClick={() => setStep(1)}>
-                    Back
-                    </button>
+                    <Canvas
+                        initialData={trinket}
+                        onSave={(dataUrl) => {
+                            setTrinket(dataUrl);
+                            setStep(2);
+                        }}
+                    />
                 </div>
-            )}
+                )}
 
-            <button onClick={onClose} 
-                className="mt-4 cursor-pointer">
-                x
-            </button>
+                {step === 2 && (
+                    <div>
+                        <p>Step 2: Add Images</p>
+                        <button
+                            onClick={() => document.getElementById("imageUpload")?.click()}
+                            className="border px-3 py-2 rounded"
+                            >
+                            + Add Image
+                        </button>
 
-            <button onClick={handleSave}>
-                v
-            </button>
+                        <div className="max-h-[40vh] overflow-y-auto">
+                            <input
+                                id="imageUpload"
+                                type="file"
+                                accept="image/*"
+                                className="hidden"
+                                onChange={handleUpload}
+                            />
+                            {images.map(img => (
+                                <div key={img.id}>
+                                    <img src={img.url} />
 
-        </div>
+                                    <input
+                                    value={img.caption || ""}
+                                    onChange={(e) =>
+                                        setImages(prev =>
+                                        prev.map(i =>
+                                            i.id === img.id
+                                            ? { ...i, caption: e.target.value }
+                                            : i
+                                        )
+                                        )
+                                    }
+                                    />
+
+                                    <button
+                                    onClick={() =>
+                                        setImages(prev =>
+                                        prev.filter(i => i.id !== img.id)
+                                        )
+                                    }
+                                    >
+                                    delete
+                                    </button>
+                                </div>
+                                
+                                ))}
+                        </div>
+                        <button onClick={() => setStep(1)}>
+                            Back
+                        </button>
+                    </div>
+                    
+                )}
+
+                <button onClick={onClose} 
+                    className="mt-4 cursor-pointer">
+                    x
+                </button>
+
+                <button onClick={handleSave}>
+                    v
+                </button>
+
+            </div>
         </div>
         
     );
